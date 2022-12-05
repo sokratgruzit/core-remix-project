0404ba233b4fa6ed3752b762fca064308efa2398
goga okradze <deathofsuper@gmail.com>
2022-12-05 14:57:57 +0400

    styled Plugin manager

--- libs/remix-ui/plugin-manager/src/lib/components/moduleHeading.tsx
+++ libs/remix-ui/plugin-manager/src/lib/components/moduleHeading.tsx
@@ -1,20 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars,no-use-before-define */
-import React from 'react'
+import React from "react";
 
 interface ModuleHeadingProps {
-  headingLabel: string
-  count: number
+  headingLabel: string;
+  count: number;
 }
 
-function ModuleHeading ({ headingLabel, count }: ModuleHeadingProps) {
+function ModuleHeading({ headingLabel, count }: ModuleHeadingProps) {
   return (
     <nav className="plugins-list-header justify-content-between navbar navbar-expand-lg bg-light navbar-light align-items-center">
-      <span className="navbar-brand plugins-list-title h6 mb-0 mr-2">{headingLabel}</span>
-      <span className="badge badge-primary" style={{ cursor: 'default' }} data-id="pluginManagerComponentInactiveTilesCount">
-        {count}
+      <span className="navbar-brand plugins-list-title h6 mb-0 font-14 text-dark">
+        {headingLabel}&nbsp;
+      </span>
+      <span
+        className="font-14"
+        style={{ cursor: "default", color: "#FF7152" }}
+        data-id="pluginManagerComponentInactiveTilesCount"
+      >
+        ({count})
       </span>
     </nav>
-  )
+  );
 }
 
-export default ModuleHeading
+export default ModuleHeading;
